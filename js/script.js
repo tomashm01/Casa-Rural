f362f0b679e65fa7da7226e1a4a4356f0c7454c1
tomashm01 <tomashidalgomartin@gmail.com>
2022-01-20 02:11:16 +0100

    feat: pagina principal con comportamiento y animaciones

---
+++ js/script.js
@@ -0,0 +1,29 @@
+{
+    document.addEventListener('DOMContentLoaded', function() {
+
+        //Comportamiento parrafos
+        let parrafos=document.getElementsByTagName('p');
+        for(let i=0;i<parrafos.length;i++){
+            parrafos[i].classList.add('ocultar');
+        }
+        let contador=0;
+
+        setInterval(comportamientoFrases,1000);
+
+        function comportamientoFrases(){
+            parrafos[contador].classList.remove('ocultar');
+            parrafos[contador].classList.add('mostrar');
+            for(let i=0;i<parrafos.length;i++){
+                if(i!=contador){
+                    parrafos[i].classList.remove('mostrar');
+                    parrafos[i].classList.add('ocultar');
+                }
+            }
+            contador++;
+            if(contador==parrafos.length){
+                contador=0;
+            }
+        }
+
+    });
+}
