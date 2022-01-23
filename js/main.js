dbedb90552d1ec2e7cea2981bb46a0e09cbceb77
tomashm01 <tomashidalgomartin@gmail.com>
2022-01-23 20:31:45 +0100

    feat: main.html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+{
+    document.addEventListener("DOMContentLoaded", function() {
+
+        const body=document.getElementsByTagName("body")[0];
+
+        //Cambiar el fondo cada 5 segundos
+        setInterval(function(){
+            const rand=Math.floor(Math.random()*4)+1;
+            body.style.backgroundImage="url('./img/fondo"+rand+".jpg')";
+        },1000);
+
+    });
+}
